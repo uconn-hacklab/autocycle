5c09f4df3be6ba81308df27522d1339839f1bd96
Vihaan <vihaan.shah@uconn.edu>
2025-02-24 21:41:23 -0500

    Setup docs

--- docs/conf.py
+++ docs/conf.py
@@ -1,55 +1,123 @@
 # Configuration file for the Sphinx documentation builder.
 #
-# For the full list of built-in configuration values, see the documentation:
+# Full list of options can be found in the Sphinx documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
-# -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+import os
+import sys
+from typing import Any, Dict
 
-project = 'autocycle'
-copyright = '2024, Vihaan Shah'
-author = 'Vihaan Shah'
+# -- Project information -----------------------------------------------------
+#
+
+project = "furo"
+copyright = "2025, Hacklab"
+author = "Vihaan Shah"
 
 # -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+#
 
 extensions = [
+    # Sphinx's own extensions
     "sphinx.ext.autodoc",
-    "sphinx.ext.coverage",
-    "sphinx-mathjax-offline",
-    "sphinx.ext.napoleon",
-    "sphinx_favicon",
+    "sphinx.ext.extlinks",
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.mathjax",
+    "sphinx.ext.todo",
+    "sphinx.ext.viewcode",
+    # Our custom extension, only meant for own documentation.
+    "furo.sphinxext",
+    # External stuff
+    "myst_parser",
+    "sphinx_copybutton",
+    "sphinx_design",
+    "sphinx_inline_tabs",
 ]
 
-autodoc_typehints = "both"
-autodoc_class_signature = "separated"
-autodoc_default_options = {
-    "members": True,
-    "member-order": "bysource",
-    "inherited-members": False,
-    "exclude-members": "__init__, __post_init__, __new__",
+# -- Options for Autodoc --------------------------------------------------------------
+
+autodoc_member_order = "bysource"
+autodoc_preserve_defaults = True
+
+# Keep the type hints outside the function signature, moving them to the
+# descriptions of the relevant function/methods.
+autodoc_typehints = "description"
+
+# -- Options for extlinks ----------------------------------------------------
+#
+
+extlinks = {
+    "pypi": ("https://pypi.org/project/%s/", "%s"),
 }
 
-templates_path = ["_templates"]
-exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+# -- Options for intersphinx -------------------------------------------------
+#
 
-source_suffix = {".rst": "restructuredtext"}
-
-pygments_style = "sphinx"
-
-autodoc_type_aliases = {
-    "npt.ArrayLike": "ArrayLike",
+intersphinx_mapping = {
+    "python": ("https://docs.python.org/3", None),
+    "sphinx": ("https://www.sphinx-doc.org/en/master", None),
 }
+
+# -- Options for TODOs -------------------------------------------------------
+#
+
+todo_include_todos = True
+
+# -- Options for Markdown files ----------------------------------------------
+#
+
+myst_enable_extensions = [
+    "colon_fence",
+    "deflist",
+]
+myst_heading_anchors = 3
 
 # -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+#
 
 html_theme = "furo"
-html_theme_options = {
-    "light_css_variables": {
-        "color-brand-primary": "red",
-        "color-brand-content": "#CC3333",
-        "color-admonition-background": "orange",
-    },
+html_title = "autocycle"
+language = "en"
+
+html_static_path = ["_static"]
+html_css_files = ["pied-piper-admonition.css"]
+
+html_theme_options: Dict[str, Any] = {
+    "footer_icons": [
+        {
+            "name": "GitHub",
+            "url": "https://github.com/uconn-hacklab/autocycle",
+            "html": """
+                <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 16 16">
+                    <path fill-rule="evenodd" d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0 0 16 8c0-4.42-3.58-8-8-8z"></path>
+                </svg>
+            """,
+            "class": "",
+        },
+    ],
+    "source_repository": "https://github.com/uconn-hacklab/autocycle",
+    "source_branch": "main",
+    "source_directory": "docs/",
 }
-html_static_path = ['_static']
+
+# -- Options for theme development -------------------------------------------
+# Make sure these are all set to the default values.
+
+html_js_files = []
+html_context: Dict[str, Any] = {}
+html_show_sphinx = False
+html_show_copyright = True
+html_last_updated_fmt = ""
+
+
+# -- Options to suppress warnings and exclude virtual environment files ----------
+
+suppress_warnings = ['toctree']
+
+exclude_patterns = [
+    '.venv/*',
+    '**/site-packages/**',
+    '**/.git/**',
+    '**/__pycache__/**',
+    '**/node_modules/**',
+]
---
+++ docs/index.md
@@ -0,0 +1,7 @@
+# Autocycle
+
+Welcome to the Autocycle an autonomous riderless bike project at UConn Hacklab.
+
+```{toctree}
+:maxdepth: 2
+:hidden:
